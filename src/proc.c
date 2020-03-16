4c5159048ba0f5bc886a4418aa90b18cdabc8373
DCchico <caodifei2021@gmail.com>
2020-03-16 01:31:19 -0400

    added context switch

--- src/proc.c
+++ src/proc.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <math.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 /*
     How-to: give the first 4 arguments to configure processes
@@ -17,14 +18,21 @@
 typedef struct
 {
     char id;            // name of the proc
-    int stat;           // status {0: new, 1: completed, 2: ready, 3: blocking, 4: running}
+    int stat;           // status {0: new, 1: completed, 2: ready, 3: blocking, 4: running, 5: context switch on to CPU, 6: context switch out of CPU}
     int cpu_b;          // number of cpu bursts it has
     int arrival_t;      // arrival time
     int cpu_t[100];     // length of each burst
     int io_t[100];      // length of each io
+    int cpu_b_static;   // reserved for final calculation
+    int arrival_t_static; // reserved for final calculation
+    int burst_t;        // keep track of burst time
+    int wait_t;         // keep track of wait time
 } Proc;
 
 double * gen_rands (int * seed, int iterations, int ub, double lambda, double * times);
+void cxt_s_in (Proc * proc[], int cs_t, int procs_num, int t);
+void cxt_s_out (Proc * proc, int cs_t);
+
 
 int gen_procs (Proc * procs, char * argv[])
 {
@@ -49,8 +57,10 @@ int gen_procs (Proc * procs, char * argv[])
         procs[i].stat = 0;
         srand48(seed);
         procs[i].cpu_b = trunc(drand48() * 100);
+        procs[i].cpu_b_static = procs[i].cpu_b;
         seed += 1;
         procs[i].arrival_t = trunc(times[t_ctr++]);
+        procs[i].arrival_t_static = procs[i].arrival_t;
         for (int j = 0; j < procs[i].cpu_b; j++)
         {   
             if (ceil(times[t_ctr]) <= ub)
@@ -108,6 +118,22 @@ int sort (const void * a, const void * b)
         return (p1->id > p2->id) - (p1->id < p2->id);
 }
 
+void get_Q (Proc * ready[], int procs_num, char * queue)
+{
+    int ctr = 0;
+    for (int i = 0; i < procs_num; i++)
+    {
+        if (ready[i] == NULL)
+            break;
+        if (ready[i]->stat == 2)
+        {
+            queue[ctr] = ready[i]->id;
+            ctr++;
+        }
+    }
+    queue[ctr] = '\0';
+}
+
 void append_ready_queue (Proc * ready_procs[], Proc * procs, int procs_num, int * ctr_ready, int t)
 {
     Proc ready[26];
@@ -115,7 +141,7 @@ void append_ready_queue (Proc * ready_procs[], Proc * procs, int procs_num, int 
     
     for (int i = 0; i < procs_num; i++)
     {
-        if (procs[i].arrival_t == t && procs[i].stat != 4)
+        if (procs[i].arrival_t == t && procs[i].stat < 4)
         {
             ready[ctr] = procs[i];
             ctr++;
@@ -125,22 +151,27 @@ void append_ready_queue (Proc * ready_procs[], Proc * procs, int procs_num, int 
         qsort(ready, ctr, sizeof(Proc), sort);
     for (int i = 0; i < ctr; i++)
     {
-#if 1
-    printf("At time %d, append ready queue[%d] = %c\n", t, *ctr_ready, ready[i].id);
-#endif
         ready_procs[*ctr_ready] = &procs[(int)ready[i].id - 65];
+        bool io = false;
+        if (ready_procs[*ctr_ready]->stat == 3)
+            io = true;
         ready_procs[*ctr_ready]->stat = 2;
+
+#if 1
+    char q[procs_num+1];
+    get_Q(ready_procs, procs_num, q);
+    printf("time <%d>ms: <Process[%c]: arrival> [Q <%s>]\n", t, ready[i].id, q);
+    if (io == true)
+        printf("time <%d>ms: <Process[%c]: finishes performing I/O> [Q <%s>]\n", t, ready[i].id, q);
+#endif
         *ctr_ready += 1;
     }
 }
 
 void FCFS_SJF_burst_begin (Proc * proc, int t)
 {
-    if (proc->stat != 2 || proc->cpu_b == 0)
+    if (proc->stat != 5 || proc->cpu_b == 0)
         perror("ERROR: <Invalid Ready Queue.>");
-#if 1
-    printf("At time %d, Process[%c] begin to burst\n", t, proc->id);
-#endif
     proc->stat = 4;
     proc->cpu_b -= 1;
     proc->arrival_t = t + proc->cpu_t[0];
@@ -152,13 +183,15 @@ void FCFS_SJF_burst_begin (Proc * proc, int t)
 
 void check_proc_completion (Proc * ready[], int procs_num, int * ctr_ready, int t)
 {
-    if (ready[0]->stat == 4 && ready[0]->arrival_t == t)
+    if (ready[0]->stat == 6 && ready[0]->arrival_t == t)
     {
         if (ready[0]->cpu_b == 0)
         {
             ready[0]->stat = 1;
 #if 1
-    printf("At time %d, Process[%c] completes all bursts\n", t, ready[0]->id);
+    char q[procs_num+1];
+    get_Q(ready, procs_num, q);
+    printf("time <%d>ms: <Process[%c]: terminates by finishing its last CPU busrt> [Q <%s>]\n", t, ready[0]->id, q);
 #endif
         }
         else
@@ -170,7 +203,10 @@ void check_proc_completion (Proc * ready[], int procs_num, int * ctr_ready, int 
                 ready[0]->io_t[i] = ready[0]->io_t[i+1];
             }
 #if 1
-    printf("At time %d, Process[%c] finishes a burst\n", t, ready[0]->id);
+    char q[procs_num+1];
+    get_Q(ready, procs_num, q);
+    printf("time <%d>ms: <Process[%c]: finishes using the CPU> [Q <%s>]\n", t, ready[0]->id, q);
+    printf("time <%d>ms: <Process[%c]: starts performing I/O> [Q <%s>]\n", t, ready[0]->id, q);
 #endif
         }
         
@@ -185,10 +221,37 @@ void check_proc_completion (Proc * ready[], int procs_num, int * ctr_ready, int 
     }
 }
 
+void cxt_s_in (Proc * proc[], int cs_t, int procs_num, int t)
+{
+    proc[0]->arrival_t += cs_t/2;
+    proc[0]->stat = 5;
+#if 1
+    char q[procs_num+1];
+    get_Q(proc, procs_num, q);
+    printf("time <%d>ms: <Process[%c]: starts using the CPU> [Q <%s>]\n", t, proc[0]->id, q);
+#endif
+}
 
+void cxt_s_out (Proc * proc, int cs_t)
+{
+    proc->arrival_t += cs_t/2;
+    proc->stat = 6;
+}
+
+void check_burst (Proc * proc, int cs_t, int t)
+{
+    if (proc->stat == 4 && proc->arrival_t == t)
+    {
+        cxt_s_out(proc, cs_t);
+    }
+}
+
+
+// ----------------------------------------------------------------
 int main (int argc, char * argv[])
 {
     int procs_num = strtol(argv[4], NULL, 10);
+    int cs_t = strtol(argv[5], NULL, 10);
     Proc procs[procs_num];
     gen_procs(procs, argv);
     // Create instances
@@ -200,7 +263,7 @@ int main (int argc, char * argv[])
     int ctr_ready = 0;
 
 #if 1
-    printf("Time starts\n");
+    printf("Start of simulation\n");
 #endif
 
     // Time starts
@@ -219,17 +282,27 @@ int main (int argc, char * argv[])
         
         // Step 2: Fill in the ready queue
         append_ready_queue(ready, procs, procs_num, &ctr_ready, t);
-        
-        // Step 3: Check if burst completes
+       
+        // Step 3: Check if CPU burst/context switch completes
         if (ready[0] != NULL)
-            check_proc_completion (ready, procs_num, &ctr_ready, t);
+        {
+            check_burst(ready[0], cs_t, t);
+            check_proc_completion(ready, procs_num, &ctr_ready, t);
+        }
 
-        // Step 4: Begin to burst
-        if (ready[0] != NULL && ready[0]->stat != 4)
-            FCFS_SJF_burst_begin(ready[0], t);
+        // Step 4: Begin to burst/context switch on to CPU
+        if (ready[0] != NULL)
+        {
+            if (ready[0]->stat == 2)
+                cxt_s_in(ready, cs_t, procs_num, t);
+            if (ready[0]->stat == 5)
+                FCFS_SJF_burst_begin(ready[0], t);
+        }
 
         t++;
     }
-
+#if 1
+    printf("End of simulation\n");
+#endif
 }
 
